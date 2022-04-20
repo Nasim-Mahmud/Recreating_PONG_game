5053c05abcbba9288823167244379bc4b0a72adb
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-04-20 22:17:16 +0600

    Modifying..

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+import time
 
 paddle = Paddle()
 
 screen = Screen()
+screen.update()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 
--- paddle.py
+++ paddle.py
@@ -1,7 +1,7 @@
 from turtle import Turtle
 
-turtle_position = [(0, 0), (0, -20), (0, 40)]
-
+turtle1_position = [(300, 0), (300, 20), (300, 40)]
+turtle2_position = [(-300, 0), (-300, 20), (-300, 40)]
 
 class Paddle:
     def __init__(self):
@@ -9,7 +9,9 @@ class Paddle:
         self.create_paddle()
 
     def create_paddle(self):
-        for position in turtle_position:
+        for position in turtle1_position:
+            self.add_paddle(position)
+        for position in turtle2_position:
             self.add_paddle(position)
 
     def add_paddle(self, pos):
