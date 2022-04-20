687f7d4c459e5899665fc20b8ba30e534d712e6f
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-04-20 22:46:53 +0600

    Adding both paddles.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
-import time
-
-paddle = Paddle()
+# import time
 
 screen = Screen()
-screen.update()
+screen.tracer(0)
+
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 
+game_is_on = True
+while game_is_on:
+    screen.update()
+    paddle = Paddle()
 screen.exitonclick()
