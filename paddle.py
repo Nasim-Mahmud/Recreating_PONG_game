b2c0ac8bda04cbcb1dfd3552c37378b07ddf7746
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-04-20 22:06:33 +0600

    Modifying..

--- paddle.py
+++ paddle.py
@@ -1,13 +1,20 @@
 from turtle import Turtle
-turtle_position = [(0,0), (0, -20), (0, 40)]
+
+turtle_position = [(0, 0), (0, -20), (0, 40)]
+
 
 class Paddle:
     def __init__(self):
         self.ggpaddle = []
+        self.create_paddle()
 
-    def add_paddle(self):
+    def create_paddle(self):
+        for position in turtle_position:
+            self.add_paddle(position)
+
+    def add_paddle(self, pos):
         dino = Turtle("square")
         dino.penup()
-        dino.color("white")
-        dino.goto()
-
+        dino.fillcolor("white")
+        dino.goto(pos)
+        self.ggpaddle.append()
