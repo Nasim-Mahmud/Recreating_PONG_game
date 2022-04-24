18617db52b9b12162778ca1d0fc0cd296f803724
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-04-24 22:39:57 +0600

    Adding moving ability to the paddles

--- paddle.py
+++ paddle.py
@@ -35,32 +35,39 @@ class Paddle:
         dino.goto(pos)
         self.ggpaddle02.append(dino)
 
-    def move_01(self):
-        for n in range(len(self.ggpaddle01)-1, 0, -1):
-            x_pos = self.ggpaddle01[n-1].xcor()
-            y_pos = self.ggpaddle01[n-1].ycor()
-            self.ggpaddle01[n].goto(x_pos, y_pos)
-        self.head01.forward(MOVE)
+    def move(self, paddles, head):
+        for n in range(len(paddles)-1, 0, -1):
+            x_pos = paddles[n-1].xcor()
+            y_pos = paddles[n-1].ycor()
+            paddles[n].goto(x_pos, y_pos)
+        head.forward(MOVE)
 
-    def move_02(self):
-        for n in range(len(self.ggpaddle02)-1, 0, -1):
-            x_pos = self.ggpaddle02[n-1].xcor()
-            y_pos = self.ggpaddle02[n-1].ycor()
-            self.ggpaddle02[n].goto(x_pos, y_pos)
-        self.head02.forward(MOVE)
+    # def move_01(self):
+    #     for n in range(len(self.ggpaddle01)-1, 0, -1):
+    #         x_pos = self.ggpaddle01[n-1].xcor()
+    #         y_pos = self.ggpaddle01[n-1].ycor()
+    #         self.ggpaddle01[n].goto(x_pos, y_pos)
+    #     self.head01.forward(MOVE)
+
+    # def move_02(self):
+    #     for n in range(len(self.ggpaddle02)-1, 0, -1):
+    #         x_pos = self.ggpaddle02[n-1].xcor()
+    #         y_pos = self.ggpaddle02[n-1].ycor()
+    #         self.ggpaddle02[n].goto(x_pos, y_pos)
+    #     self.head02.forward(MOVE)
 
     def up_arrow(self):
         self.head01.setheading(UP)
-        self.move_01()
+        self.move(self.ggpaddle01, self.head01)
 
     def up_alph(self):
         self.head02.setheading(UP)
-        self.move_02()
+        self.move(self.ggpaddle02, self.head02)
 
     def down_arrow(self):
         self.head01.setheading(DOWN)
-        self.move_01()
+        self.move(self.ggpaddle01, self.head01)
 
     def down_alph(self):
         self.head02.setheading(DOWN)
-        self.move_02()
+        self.move(self.ggpaddle02, self.head02)
