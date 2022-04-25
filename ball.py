2bf5d4bea7c1caa5fb7c511cdc55ff6c27dd270c
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-04-25 22:11:23 +0600

    Updating

--- ball.py
+++ ball.py
@@ -1,11 +1,12 @@
-# from turtle import Turtle
-#
-# class Ball:
-#
-#     def __init__(self):
-#         self.create_ball()
-#
-#     def create_ball(self):
-#         tim = Turtle("circle")
-#         tim.color("white")
-#
+from turtle import Turtle
+
+
+class Ball:
+
+    def __init__(self):
+        self.create_ball()
+
+    def create_ball(self):
+        tim = Turtle("circle")
+        tim.color("white")
+
