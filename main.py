bdc4b44bc921af9d22a23c3397c19bb8ae7c5119
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-04-25 21:55:37 +0600

    Updating

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)
 
 paddle = Turtle()
 paddle.shape("square")
@@ -11,8 +12,6 @@ paddle.color("white")
 paddle.shapesize(stretch_wid=5, stretch_len=1)
 paddle.penup()
 paddle.goto(350, 0)
-
-screen.tracer(0)
 
 
 def go_up():
@@ -32,4 +31,5 @@ game_is_on = True
 
 while game_is_on:
     screen.update()
+
 screen.exitonclick()
