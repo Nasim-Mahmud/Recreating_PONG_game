7492dac0ad1e4a9b11ef61b0566dd3542af2ae94
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-04-27 22:24:23 +0600

    Adding collision mechanics with paddles.

--- main.py
+++ main.py
@@ -26,8 +26,9 @@ while game_is_on:
     screen.update()
     ball.move()
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
 
-#     Detect collision with paddle
-
+#     Detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340:
+        print("contact")
 screen.exitonclick()
