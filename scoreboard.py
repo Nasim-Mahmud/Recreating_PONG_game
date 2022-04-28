693ccc040c8f214d430b6b3dbf8a4db50dc8b0ea
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-04-29 00:21:39 +0600

    Adding collision mechanics with paddles.

--- scoreboard.py
+++ scoreboard.py
@@ -9,4 +9,7 @@ class Scoreboard(Turtle):
         self.ht()
         self.l_score = 0
         self.r_score = 0
-        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+        self.goto(-100, 240)
+        self.write(self.l_score, align="center", font=("Courier", 40, "bold"))
+        self.goto(100, 240)
+        self.write(self.r_score, align="center", font=("Courier", 40, "bold"))
