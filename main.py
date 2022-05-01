c609f44847af375e501a5c7d77a504efd496fc97
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-02 01:03:02 +0600

    Setting level.

--- main.py
+++ main.py
@@ -22,7 +22,10 @@ screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
-game_is_on = True
+game_is_on = False
+
+if level:
+    game_is_on = True
 
 while game_is_on:
     time.sleep(ball.move_speed)
