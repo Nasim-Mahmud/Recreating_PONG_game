5d4d2e2372cb9b4aabd0dd962080f9dcb9dec523
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-08 22:52:25 +0600

    Setting level.

--- main.py
+++ main.py
@@ -36,8 +36,15 @@ game_is_on = False
 #     level = screen.textinput(title="Welcome to Pong!", prompt="What will be the highest score?")
 #     game_is_on = False
 
-while int(level) != 0:
-    game_is_on
+ask = True
+while ask:
+    if int(level) > 0:
+        game_is_on = True
+        ask = False
+    else:
+        level = screen.textinput(title="Welcome to Pong!", prompt="What will be the highest score?")
+        game_is_on = False
+        ask = True
 
 while game_is_on:
     time.sleep(ball.move_speed)
