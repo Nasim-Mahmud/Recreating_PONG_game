6640e6e91ee24c01dc0c1e67704adb4c34511ded
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-10 22:01:41 +0600

    Setting level and debugging.

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
-game_is_on = True
+
 
 # Game mechanics
 while game_is_on:
